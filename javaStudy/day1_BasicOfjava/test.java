142284940e8e500a8c7b428b57fc74dfddaa6f2f
changin <dain0126@naver.com>
2025-08-23 11:10:13 +0900

    day1 complete

---
+++ javaStudy/day1_BasicOfjava/test.java
@@ -0,0 +1,10 @@
+class test{
+    public static void main(String args[]){
+        for(int i=0;i<=9;i++){
+            for(int j=0;j<=9;j++){
+                if((10*i + j) + (10*j + i) == 99)
+                    System.out.println("i : "+i+","+"j : "+j);
+            }
+        }
+    }
+}
