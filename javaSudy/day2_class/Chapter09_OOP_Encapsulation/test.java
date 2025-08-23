b0464d877ab9fed9b4f1df486123919873fb06b7
changin <dain0126@naver.com>
2025-08-24 02:08:33 +0900

    still Chapter 09

---
+++ javaSudy/day2_class/Chapter09_OOP_Encapsulation/test.java
@@ -0,0 +1,18 @@
+/*
+ * Chapter09 Encapsulation 
+ * 
+ * 정보은닉/캡슐화
+ * 
+ * 접근 수준 지시자(Acess-level Modifiers)
+ * public, protected, defualt, private
+ * 
+ * class 대상 : public, defualt 가능.
+ * filed와 method 대상 : public, protected, defualt, private 가능.
+ */
+
+class test{
+    public static void main(String args[]){
+
+    }
+
+}
