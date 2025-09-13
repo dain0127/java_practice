f427c2859a88de10203a8e624d9fa0043b20335c
changin <dain0126@naver.com>
2025-09-14 01:09:32 +0900

    Day 10 start

--- javaStudy/day10_IOStream/test.java
+++ javaStudy/day10_IOStream/test.java
@@ -1,1 +1,12 @@
-hello 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+class test{
+    public static void main(String[] args) throws IOException {
+        OutputStream out = new FileOutputStream("data.dat");
+        InputStream in = new FileInputStream("data.dat");
+    }
+}
